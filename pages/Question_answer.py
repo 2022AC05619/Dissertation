12181d82b7494de8eb34237552d6b167986b1540
2022AC05619 <2022AC05619@WILP.BITS-PILANI.AC.IN>
2025-02-26 22:48:20 +0530

    Update Question_answer.py

--- pages/Question_answer.py
+++ pages/Question_answer.py
@@ -11,7 +11,59 @@ from langchain.prompts import PromptTemplate
 import streamlit as st
 from langchain.chains import RetrievalQA
 from langchain_core.messages import HumanMessage, AIMessage
+from langchain.retrievers import EnsembleRetriever
+from langchain_community.retrievers import BM25Retriever
+from rank_bm25 import BM25Okapi
+from llama_index.core import VectorStoreIndex
+import os
 
+def combined_context(vector_store,keyword_retriever, query, k=10):
+    retriever_vectordb = vector_store.as_retriever(search_kwargs={"k": k})
+    keyword_retriever.k = k
+    ensemble_retriever = EnsembleRetriever(retrievers=[retriever_vectordb,keyword_retriever],
+                                       weights=[0.5, 0.5])
+    docs_rel=ensemble_retriever.get_relevant_documents(query)
+
+    context_data = ""
+    for i in range(len(docs_rel)):
+        context_data += docs_rel[i].page_content + "\n\n"
+    
+    return context_data
+
+
+# Create chatbot function with hybrid retrieval
+def chatbot_response(vector_store,keyword_retriever , llm, query,):
+    context = combined_context(vector_store,keyword_retriever, query, k=10)
+    prompt_text = prompt.format(context=context, question=query)
+    # print(prompt_text)
+    response = llm.invoke(prompt_text)
+    return response
+
+
+
+
+def FAISS_file_exists(file_path):
+    for filename in os.listdir(file_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):  # Ensure it's a file, not a directory
+            print(file_path)
+
+
+def FAISS_file_exists(file_path):
+    file_ind = 0
+    for filename in os.listdir(file_path):
+        # print(filename)
+        if filename == 'index.faiss':
+            file_ind = 1
+            break
+        else:
+            file_ind = 0
+    return file_ind
+
+
+
+file_location = "F:\Bits\Sem 4 - Dissertation"
+Faiss_ind = FAISS_file_exists(file_location)
 # Display welcome message
 if "output_path" in st.session_state:
     st.write("Hello, Welcome to the Question and Answer Page!")
@@ -26,16 +78,27 @@ status_text = st.empty()
 
 # Preprocess the markdown document
 texts = markdown_preprocessing.preprocess_markdow_doc(input_file_path)
+new_texts = markdown_preprocessing.text_chunking(texts,1000)
 
-progress_bar.progress(20)
+progress_bar.progress(30)
 status_text.text("Processing the Document")
 
 # Convert text into LangChain Documents
-texts = [Document(page_content=text) for text in texts]
+texts_new = [Document(page_content=new_texts[i]) for i in range(len(new_texts))]
 
 # Generate embeddings and create FAISS index
-embeddings = OllamaEmbeddings(model="mistral:latest")
-vector_store = FAISS.from_documents(texts, embeddings)
+if Faiss_ind == 1:
+    print("File found")
+    embeddings = OllamaEmbeddings(model="mistral:latest")
+    vector_store = FAISS.load_local(file_location,embeddings,allow_dangerous_deserialization=True)
+    keyword_retriever = BM25Retriever.from_documents(texts_new)
+
+else:
+    print("File not found")
+    embeddings = OllamaEmbeddings(model="mistral:latest")
+    vector_store = FAISS.from_documents(texts_new, embeddings)
+    keyword_retriever = BM25Retriever.from_documents(texts_new)
+    vector_store.save_local(file_location)
 
 progress_bar.progress(100)
 status_text.text("Document Processing Complete")
@@ -44,8 +107,13 @@ status_text.text("Document Processing Complete")
 custom_prompt_template = """You are a personalised Document Analyser. Use the following information to answer the user's question.
 If you don't know the answer, just say that you don't know.
 
-Context: {context}
-Question: {question}
+Context: 
+
+{context}
+
+Question: 
+
+{question}
 
 Only return the helpful answer below.
 Helpful answer:
@@ -56,13 +124,6 @@ prompt = PromptTemplate(template=custom_prompt_template, input_variables=['conte
 # Initialize LLM and retrieval-based QA
 llm = ChatOllama(model="mistral:latest", base_url="http://localhost:11434", temperature=0)
 
-qa_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type='stuff',
-    retriever=vector_store.as_retriever(search_kwargs={'k': 3}),
-    return_source_documents=True,
-    chain_type_kwargs={'prompt': prompt}
-)
 
 st.write("Chatbot is ready! Start asking questions below.")
 
@@ -75,8 +136,8 @@ if "chat_history" not in st.session_state:
 # Chat Input
 user_input = st.chat_input("Type your message here...")
 if user_input and user_input.strip():
-    response_dict = qa_chain.invoke(user_input)  # Returns {'result': ..., 'source_documents': ...}
-    response = response_dict["result"]  # Extract only the answer
+    response_dict = chatbot_response(vector_store,keyword_retriever, llm, user_input)  # Returns {'result': ..., 'source_documents': ...}
+    response = response_dict.content  # Extract only the answer
 
     # Append conversation to history
     st.session_state.chat_history.append(HumanMessage(content=user_input))
